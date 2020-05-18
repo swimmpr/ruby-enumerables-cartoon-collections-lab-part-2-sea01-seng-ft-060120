5cd365a308f358eea9260cd1c09a73e578560cd9
Marshall Raiskin <marshall.raiskin@gmail.com>
2020-05-18 20:40:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,6 +28,4 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
 
 planeteer_calls.find {|planeteer_calls| valid_calls.include? planeteer_calls }
-
-
 end
